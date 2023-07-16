afd90a51408886b2942c842971aa85def719acaa
Francis Beeson <leobeeson@gmail.com>
2023-07-16 22:16:54 +0000

    Update logger to only log to file and not std out.

--- src/loggers/log_utils.py
+++ src/loggers/log_utils.py
@@ -5,10 +5,12 @@ from datetime import datetime
 
 def setup_logger(name):
     logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
     if name == "":
-        coloredlogs.install(level="INFO", logger=logger)
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         file_handler = logging.FileHandler(f"logs/logger_{timestamp}.log")
-        file_handler.setFormatter(logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s"))
+        file_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")
+        file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)    
     return logger
