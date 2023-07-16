6737a8946eac31698e937f699b1767246afb0901
Francis Beeson <leobeeson@gmail.com>
2023-07-16 22:16:25 +0000

    Update timing method wrapper.

--- src/utils/telemetry.py
+++ src/utils/telemetry.py
@@ -18,3 +18,20 @@ def timeit(method):
             logger.info('%r  %2.2f secs' % (method.__name__, (te - ts)))
         return result
     return timed
+
+
+def time_category_iteration(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            # assumes that the first argument to the method is 'category'
+            category = args[1] if len(args) > 1 else 'no category provided'
+            logger.info('%r (%r)  %2.2f secs' % (method.__name__, category, (te - ts)))
+        return result
+    return timed
